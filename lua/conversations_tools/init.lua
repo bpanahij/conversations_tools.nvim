c5abe4425b691b8322686b59a68b152c173b0a44
Brian Johnson <brian.johnson@tavus.io>
2024-09-02 10:34:52 -0700

    removing prints

--- lua/conversations_tools/init.lua
+++ lua/conversations_tools/init.lua
@@ -4,12 +4,9 @@ function M.setup(opts)
 	opts = opts or {}
 	M.base_url = opts.base_url
 		or "https://us-west-1.console.aws.amazon.com/cloudwatch/home?region=us-west-1#logsV2:log-groups/log-group/CarterLogGroup/log-events/{REPLACE_THIS_STRING}$3Fstart$3D-345600000"
-	print("Plugin setup complete. Base URL: " .. M.base_url)
 end
 
 function M.insert_url_with_selection()
-	print("Function insert_url_with_selection called")
-
 	-- Get the visually selected text
 	local start_pos = vim.fn.getpos("'<")
 	local end_pos = vim.fn.getpos("'>")
@@ -38,11 +35,8 @@ function M.insert_url_with_selection()
 		selected_text = table.concat(lines, "\n")
 	end
 
-	print("Selected text: " .. selected_text)
-
 	-- Replace the placeholder with the selected text
 	local final_url = string.gsub(M.base_url, "{REPLACE_THIS_STRING}", selected_text)
-	print("Final URL: " .. final_url)
 
 	-- Get the current cursor position
 	local cursor = vim.api.nvim_win_get_cursor(0)
@@ -51,11 +45,8 @@ function M.insert_url_with_selection()
 	-- Insert a new line below the current line and put the URL there
 	vim.api.nvim_buf_set_lines(0, row, row, false, { "" })
 	vim.api.nvim_buf_set_lines(0, row, row + 1, false, { final_url })
-
 	-- Move the cursor to the end of the inserted URL
 	vim.api.nvim_win_set_cursor(0, { row + 1, 0 })
-
-	print("URL inserted on new line below")
 end
 
 -- Set up the command
